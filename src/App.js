b04060c433c38f356cd54c1259715e66805a4b6f
tmckenna19 <tdlmckenna@gmail.com>
2023-09-06 16:28:29 -0500

    home page finished, no links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Home from './Components/Home';
-import PickNumber from './Components/PickNumber';
 
 function App() {
   return (
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,13 +3,18 @@ import coinToss from '../Images/coinToss.jpeg';
 import war from '../Images/war.jpeg';
 import rps from '../Images/rps.jpeg';
 import hands from '../Images/hands.jpeg';
+import tikTakToe from '../Images/tikTakToe.jpeg';
+import neon from '../Images/neon.mp4';
 
 const Home = () => {
     return (
         <>
-        <h1>Welcome to Cousin Eddie's Casino</h1>
-        <p>We don't have them games you'll find at them regular casinos</p>
-        <div className="games-container">
+        <div className="header">
+            <video src={neon} autoPlay loop muted />
+            <h1>Cousin Eddie's Casino</h1>
+            <p>We don't have them games you'll find at them regular casinos</p>  
+        </div>
+        <div className="game-container">
             <div className="game">
                 <img src={numbers} alt="numbers" />
                 <h4>Pick a Number</h4>
@@ -24,11 +29,15 @@ const Home = () => {
             </div>
             <div className="game">
                 <img src={rps} alt="rock paper scissor" />
-                <h4>Rock Paper scissor</h4>
+                <h4>Rock Paper Scissor</h4>
             </div>
             <div className="game">
                 <img src={hands} alt="fists" />
                 <h4>Pick a Hand</h4>
+            </div>
+            <div className="game">
+                <img src={tikTakToe} alt="tik tak toe" />
+                <h4>Tik Tak Toe</h4>
             </div>
         </div>
         </>
