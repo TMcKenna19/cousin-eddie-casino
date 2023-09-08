4ab1244beb3a6e9dc1f1b2a3b1c83f261968695e
tmckenna19 <tdlmckenna@gmail.com>
2023-09-08 11:38:49 -0500

    started logic for pick a number

--- src/Components/PickNumber.jsx
+++ src/Components/PickNumber.jsx
@@ -1,8 +1,33 @@
-
+import { useState } from "react";
 
 const PickNumber = () => {
+    const [playerBalance, setPlayerBalance] = useState(25);
+    const [playerBet, setPlayerBet] = useState();
+    const [number, setNumber] = useState();
+    const [playerNumber, setPlayerNumber] = useState();
+
+    //Get random number
+    const getNumber = () => {
+        return Math.floor(Math.random() * 10) + 1;
+    }
+   
+    const handleClick = () => {
+        setNumber(getNumber);
+        if(playerNumber = getNumber) {
+            playerBet + playerBalance
+        }
+        console.log(number)
+    }
+
+
     return(
+        <>
         <h1>Pick A Number</h1>
+        <p>${playerBalance}</p>
+        <p>Dealer Number: {number}</p>
+        <button onClick={handleClick}>Get Number</button>
+        
+        </>
     )
 }
 
